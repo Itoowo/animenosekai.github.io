098ecde1f3a5dd77b44d813adb34b5b9238da4c8
Itoowo <kuragari.ito@gmail.com>
2022-09-09 00:17:44 +0700

    Added new files

---
+++ js/popuptrailer.js
@@ -0,0 +1,568 @@
+const show1 = document.querySelector('.smallboxa1');
+const show2 = document.querySelector('.smallboxa2');
+const show3 = document.querySelector('.smallboxa3');
+const show4 = document.querySelector('.smallboxa4');
+const show5 = document.querySelector('.smallboxa5');
+const show6 = document.querySelector('.smallboxb1');
+const show7 = document.querySelector('.smallboxb2');
+const show8 = document.querySelector('.smallboxb3');
+const show9 = document.querySelector('.smallboxb4');
+const show10 = document.querySelector('.smallboxb5');
+const show11 = document.querySelector('.smallboxc1');
+const show12 = document.querySelector('.smallboxc2');
+const show13 = document.querySelector('.smallboxc3');
+const show14 = document.querySelector('.smallboxc4');
+const show15 = document.querySelector('.smallboxc5');
+const show16 = document.querySelector('.smallboxd1');
+const show17 = document.querySelector('.smallboxd2');
+const show18 = document.querySelector('.smallboxd3');
+const show19 = document.querySelector('.smallboxd4');
+const show20 = document.querySelector('.smallboxd5');
+const show21 = document.querySelector('.smallboxe1');
+const show22 = document.querySelector('.smallboxe2');
+const show23 = document.querySelector('.smallboxe3');
+const show24 = document.querySelector('.smallboxe4');
+const show25 = document.querySelector('.smallboxe5');
+const show26 = document.querySelector('.smallboxf1');
+const show27 = document.querySelector('.smallboxf2');
+const show28 = document.querySelector('.smallboxf3');
+const show29 = document.querySelector('.smallboxf4');
+const show30 = document.querySelector('.smallboxf5');
+const show31 = document.querySelector('.smallboxg1');
+const show32 = document.querySelector('.smallboxg2');
+const show33 = document.querySelector('.smallboxg3');
+const show34 = document.querySelector('.smallboxg4');
+const show35 = document.querySelector('.smallboxg5');
+const show36 = document.querySelector('.smallboxh1');
+const show37 = document.querySelector('.smallboxh2');
+const show38 = document.querySelector('.smallboxh3');
+const show39 = document.querySelector('.smallboxh4');
+const show40 = document.querySelector('.smallboxh5');
+const trailer1 = document.querySelector('.anime_trailer1');
+const trailer2 = document.querySelector('.anime_trailer2');
+const trailer3 = document.querySelector('.anime_trailer3');
+const trailer4 = document.querySelector('.anime_trailer4');
+const trailer5 = document.querySelector('.anime_trailer5');
+const trailer6 = document.querySelector('.anime_trailer6');
+const trailer7 = document.querySelector('.anime_trailer7');
+const trailer8 = document.querySelector('.anime_trailer8');
+const trailer9 = document.querySelector('.anime_trailer9');
+const trailer10 = document.querySelector('.anime_trailer10');
+const trailer11 = document.querySelector('.anime_trailer11');
+const trailer12 = document.querySelector('.anime_trailer12');
+const trailer13 = document.querySelector('.anime_trailer13');
+const trailer14 = document.querySelector('.anime_trailer14');
+const trailer15 = document.querySelector('.anime_trailer15');
+const trailer16 = document.querySelector('.anime_trailer16');
+const trailer17 = document.querySelector('.anime_trailer17');
+const trailer18 = document.querySelector('.anime_trailer18');
+const trailer19 = document.querySelector('.anime_trailer19');
+const trailer20 = document.querySelector('.anime_trailer20');
+const trailer21 = document.querySelector('.anime_trailer21');
+const trailer22 = document.querySelector('.anime_trailer22');
+const trailer23 = document.querySelector('.anime_trailer23');
+const trailer24 = document.querySelector('.anime_trailer24');
+const trailer25 = document.querySelector('.anime_trailer25');
+const trailer26 = document.querySelector('.anime_trailer26');
+const trailer27 = document.querySelector('.anime_trailer27');
+const trailer28 = document.querySelector('.anime_trailer28');
+const trailer29 = document.querySelector('.anime_trailer29');
+const trailer30 = document.querySelector('.anime_trailer30');
+const trailer31 = document.querySelector('.anime_trailer31');
+const trailer32 = document.querySelector('.anime_trailer32');
+const trailer33 = document.querySelector('.anime_trailer33');
+const trailer34 = document.querySelector('.anime_trailer34');
+const trailer35 = document.querySelector('.anime_trailer35');
+const trailer36 = document.querySelector('.anime_trailer36');
+const trailer37 = document.querySelector('.anime_trailer37');
+const trailer38 = document.querySelector('.anime_trailer38');
+const trailer39 = document.querySelector('.anime_trailer39');
+const trailer40 = document.querySelector('.anime_trailer40');
+const close1 = document.querySelector('.close1');
+const close2 = document.querySelector('.close2');
+const close3 = document.querySelector('.close3');
+const close4 = document.querySelector('.close4');
+const close5 = document.querySelector('.close5');
+const close6 = document.querySelector('.close6');
+const close7 = document.querySelector('.close7');
+const close8 = document.querySelector('.close8');
+const close9 = document.querySelector('.close9');
+const close10 = document.querySelector('.close10');
+const close11 = document.querySelector('.close11');
+const close12 = document.querySelector('.close12');
+const close13 = document.querySelector('.close13');
+const close14 = document.querySelector('.close14');
+const close15 = document.querySelector('.close15');
+const close16 = document.querySelector('.close16');
+const close17 = document.querySelector('.close17');
+const close18 = document.querySelector('.close18');
+const close19 = document.querySelector('.close19');
+const close20 = document.querySelector('.close20');
+const close21 = document.querySelector('.close21');
+const close22 = document.querySelector('.close22');
+const close23 = document.querySelector('.close23');
+const close24 = document.querySelector('.close24');
+const close25 = document.querySelector('.close25');
+const close26 = document.querySelector('.close26');
+const close27 = document.querySelector('.close27');
+const close28 = document.querySelector('.close28');
+const close29 = document.querySelector('.close29');
+const close30 = document.querySelector('.close30');
+const close31 = document.querySelector('.close31');
+const close32 = document.querySelector('.close32');
+const close33 = document.querySelector('.close33');
+const close34 = document.querySelector('.close34');
+const close35 = document.querySelector('.close35');
+const close36 = document.querySelector('.close36');
+const close37 = document.querySelector('.close37');
+const close38 = document.querySelector('.close38');
+const close39 = document.querySelector('.close39');
+const close40 = document.querySelector('.close40');
+var video1 = document.querySelector('.video1');
+var video2 = document.querySelector('.video2');
+var video3 = document.querySelector('.video3');
+var video4 = document.querySelector('.video4');
+var video5 = document.querySelector('.video5');
+var video6 = document.querySelector('.video6');
+var video7 = document.querySelector('.video7');
+var video8 = document.querySelector('.video8');
+var video9 = document.querySelector('.video9');
+var video10 = document.querySelector('.video10');
+var video11 = document.querySelector('.video11');
+var video12 = document.querySelector('.video12');
+var video13 = document.querySelector('.video13');
+var video14 = document.querySelector('.video14');
+var video15 = document.querySelector('.video15');
+var video16 = document.querySelector('.video16');
+var video17 = document.querySelector('.video17');
+var video18 = document.querySelector('.video18');
+var video19 = document.querySelector('.video19');
+var video20 = document.querySelector('.video20');
+var video21 = document.querySelector('.video21');
+var video22 = document.querySelector('.video22');
+var video23 = document.querySelector('.video23');
+var video24 = document.querySelector('.video24');
+var video25 = document.querySelector('.video25');
+var video26 = document.querySelector('.video26');
+var video27 = document.querySelector('.video27');
+var video28 = document.querySelector('.video28');
+var video29 = document.querySelector('.video29');
+var video30 = document.querySelector('.video30');
+var video31 = document.querySelector('.video31');
+var video32 = document.querySelector('.video32');
+var video33 = document.querySelector('.video33');
+var video34 = document.querySelector('.video34');
+var video35 = document.querySelector('.video35');
+var video36 = document.querySelector('.video36');
+var video37 = document.querySelector('.video37');
+var video38 = document.querySelector('.video38');
+var video39 = document.querySelector('.video39');
+var video40 = document.querySelector('.video40');
+
+show1.addEventListener('click',()=>{
+    trailer1.classList.add('show1');
+
+});
+
+show2.addEventListener('click',()=>{
+    trailer2.classList.add('show2');
+
+});
+
+show3.addEventListener('click',()=>{
+    trailer3.classList.add('show3');
+
+});
+
+show4.addEventListener('click',()=>{
+    trailer4.classList.add('show4');
+
+});
+
+show5.addEventListener('click',()=>{
+    trailer5.classList.add('show5');
+});
+
+show6.addEventListener('click',()=>{
+    trailer6.classList.add('show6');
+
+});
+
+show7.addEventListener('click',()=>{
+    trailer7.classList.add('show7');
+
+});
+
+show8.addEventListener('click',()=>{
+    trailer8.classList.add('show8');
+
+});
+
+show9.addEventListener('click',()=>{
+    trailer9.classList.add('show9');
+
+});
+
+show10.addEventListener('click',()=>{
+    trailer10.classList.add('show10');
+});
+
+show11.addEventListener('click',()=>{
+    trailer11.classList.add('show11');
+});
+
+show12.addEventListener('click',()=>{
+    trailer12.classList.add('show12');
+});
+
+show13.addEventListener('click',()=>{
+    trailer13.classList.add('show13');
+});
+
+show14.addEventListener('click',()=>{
+    trailer14.classList.add('show14');
+});
+
+show15.addEventListener('click',()=>{
+    trailer15.classList.add('show15');
+});
+
+show16.addEventListener('click',()=>{
+    trailer16.classList.add('show16');
+});
+
+show17.addEventListener('click',()=>{
+    trailer17.classList.add('show17');
+});
+
+show18.addEventListener('click',()=>{
+    trailer18.classList.add('show18');
+});
+
+show19.addEventListener('click',()=>{
+    trailer19.classList.add('show19');
+});
+
+show20.addEventListener('click',()=>{
+    trailer20.classList.add('show20');
+});
+
+show21.addEventListener('click',()=>{
+    trailer21.classList.add('show21');
+});
+
+show22.addEventListener('click',()=>{
+    trailer22.classList.add('show22');
+});
+
+show23.addEventListener('click',()=>{
+    trailer23.classList.add('show23');
+});
+
+show24.addEventListener('click',()=>{
+    trailer24.classList.add('show24');
+});
+
+show25.addEventListener('click',()=>{
+    trailer25.classList.add('show25');
+});
+
+show26.addEventListener('click',()=>{
+    trailer26.classList.add('show26');
+});
+
+show27.addEventListener('click',()=>{
+    trailer27.classList.add('show27');
+});
+
+show28.addEventListener('click',()=>{
+    trailer28.classList.add('show28');
+});
+
+show29.addEventListener('click',()=>{
+    trailer29.classList.add('show29');
+});
+
+show30.addEventListener('click',()=>{
+    trailer30.classList.add('show30');
+});
+
+show31.addEventListener('click',()=>{
+    trailer31.classList.add('show31');
+});
+
+show32.addEventListener('click',()=>{
+    trailer32.classList.add('show32');
+});
+
+show33.addEventListener('click',()=>{
+    trailer33.classList.add('show33');
+});
+
+show34.addEventListener('click',()=>{
+    trailer34.classList.add('show34');
+});
+
+show35.addEventListener('click',()=>{
+    trailer35.classList.add('show35');
+});
+
+show36.addEventListener('click',()=>{
+    trailer36.classList.add('show36');
+});
+
+show37.addEventListener('click',()=>{
+    trailer37.classList.add('show37');
+});
+
+show38.addEventListener('click',()=>{
+    trailer38.classList.add('show38');
+});
+
+show39.addEventListener('click',()=>{
+    trailer39.classList.add('show39');
+});
+
+show40.addEventListener('click',()=>{
+    trailer40.classList.add('show40');
+});
+
+close1.addEventListener('click',()=>{
+    trailer1.classList.remove('show1');
+    video1.pause();
+    video1.currentTime = 0;
+});
+
+close2.addEventListener('click',()=>{
+    trailer2.classList.remove('show2');
+    video2.pause();
+    video2.currentTime = 0;
+})
+
+close3.addEventListener('click',()=>{
+    trailer3.classList.remove('show3');
+    video3.pause();
+    video3.currentTime = 0;
+})
+
+close4.addEventListener('click',()=>{
+    trailer4.classList.remove('show4');
+    video4.pause();
+    video4.currentTime = 0;
+})
+
+close5.addEventListener('click',()=>{
+    trailer5.classList.remove('show5');
+    video5.pause();
+    video5.currentTime = 0;
+})
+
+close6.addEventListener('click',()=>{
+    trailer6.classList.remove('show6');
+    video6.pause();
+    video6.currentTime = 0;
+});
+
+close7.addEventListener('click',()=>{
+    trailer7.classList.remove('show7');
+    video7.pause();
+    video7.currentTime = 0;
+})
+
+close8.addEventListener('click',()=>{
+    trailer8.classList.remove('show8');
+    video8.pause();
+    video8.currentTime = 0;
+})
+
+close9.addEventListener('click',()=>{
+    trailer9.classList.remove('show9');
+    video9.pause();
+    video9.currentTime = 0;
+})
+
+close10.addEventListener('click',()=>{
+    trailer10.classList.remove('show10');
+    video10.pause();
+    video10.currentTime = 0;
+})
+
+close11.addEventListener('click',()=>{
+    trailer11.classList.remove('show11');
+    video11.pause();
+    video11.currentTime = 0;
+});
+
+close12.addEventListener('click',()=>{
+    trailer12.classList.remove('show12');
+    video12.pause();
+    video12.currentTime = 0;
+});
+
+close13.addEventListener('click',()=>{
+    trailer13.classList.remove('show13');
+    video13.pause();
+    video13.currentTime = 0;
+});
+
+close14.addEventListener('click',()=>{
+    trailer14.classList.remove('show14');
+    video14.pause();
+    video14.currentTime = 0;
+});
+
+close15.addEventListener('click',()=>{
+    trailer15.classList.remove('show15');
+    video15.pause();
+    video15.currentTime = 0;
+});
+
+close16.addEventListener('click',()=>{
+    trailer16.classList.remove('show16');
+    video16.pause();
+    video16.currentTime = 0;
+});
+
+close17.addEventListener('click',()=>{
+    trailer17.classList.remove('show17');
+    video17.pause();
+    video17.currentTime = 0;
+});
+
+close18.addEventListener('click',()=>{
+    trailer18.classList.remove('show18');
+    video18.pause();
+    video18.currentTime = 0;
+});
+
+close19.addEventListener('click',()=>{
+    trailer19.classList.remove('show19');
+    video19.pause();
+    video19.currentTime = 0;
+});
+
+close20.addEventListener('click',()=>{
+    trailer20.classList.remove('show20');
+    video20.pause();
+    video20.currentTime = 0;
+});
+
+close21.addEventListener('click',()=>{
+    trailer21.classList.remove('show21');
+    video21.pause();
+    video21.currentTime = 0;
+});
+
+close22.addEventListener('click',()=>{
+    trailer22.classList.remove('show22');
+    video22.pause();
+    video22.currentTime = 0;
+});
+
+close23.addEventListener('click',()=>{
+    trailer23.classList.remove('show23');
+    video23.pause();
+    video23.currentTime = 0;
+});
+
+close24.addEventListener('click',()=>{
+    trailer24.classList.remove('show24');
+    video24.pause();
+    video24.currentTime = 0;
+});
+
+close25.addEventListener('click',()=>{
+    trailer25.classList.remove('show25');
+    video25.pause();
+    video25.currentTime = 0;
+});
+
+close26.addEventListener('click',()=>{
+    trailer26.classList.remove('show26');
+    video26.pause();
+    video26.currentTime = 0;
+});
+
+close27.addEventListener('click',()=>{
+    trailer27.classList.remove('show27');
+    video27.pause();
+    video27.currentTime = 0;
+});
+
+close28.addEventListener('click',()=>{
+    trailer28.classList.remove('show28');
+    video28.pause();
+    video28.currentTime = 0;
+});
+
+close29.addEventListener('click',()=>{
+    trailer29.classList.remove('show29');
+    video29.pause();
+    video29.currentTime = 0;
+});
+
+close30.addEventListener('click',()=>{
+    trailer30.classList.remove('show30');
+    video30.pause();
+    video30.currentTime = 0;
+});
+
+close31.addEventListener('click',()=>{
+    trailer31.classList.remove('show31');
+    video31.pause();
+    video31.currentTime = 0;
+});
+
+close32.addEventListener('click',()=>{
+    trailer32.classList.remove('show32');
+    video32.pause();
+    video32.currentTime = 0;
+});
+
+close33.addEventListener('click',()=>{
+    trailer33.classList.remove('show33');
+    video33.pause();
+    video33.currentTime = 0;
+});
+
+close34.addEventListener('click',()=>{
+    trailer34.classList.remove('show34');
+    video34.pause();
+    video34.currentTime = 0;
+});
+
+close35.addEventListener('click',()=>{
+    trailer35.classList.remove('show35');
+    video35.pause();
+    video35.currentTime = 0;
+});
+
+close36.addEventListener('click',()=>{
+    trailer36.classList.remove('show36');
+    video36.pause();
+    video36.currentTime = 0;
+});
+
+close37.addEventListener('click',()=>{
+    trailer37.classList.remove('show37');
+    video37.pause();
+    video37.currentTime = 0;
+});
+
+close38.addEventListener('click',()=>{
+    trailer38.classList.remove('show38');
+    video38.pause();
+    video38.currentTime = 0;
+});
+
+close39.addEventListener('click',()=>{
+    trailer39.classList.remove('show39');
+    video39.pause();
+    video39.currentTime = 0;
+});
+
+close40.addEventListener('click',()=>{
+    trailer40.classList.remove('show40');
+    video40.pause();
+    video40.currentTime = 0;
+});
