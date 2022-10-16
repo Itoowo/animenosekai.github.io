4be46a45601241c495bd2f582c6303d18f6bd159
Itoowo <kuragari.ito@gmail.com>
2022-10-16 15:12:22 +0700

    update

---
+++ js/pfp.js
@@ -0,0 +1,13 @@
+document.getElementById('pfp_button').addEventListener('click', function() {
+document.querySelector('.bg-modal').style.display = 'flex';
+});
+document.querySelector('.close-btn').addEventListener('click', function(){
+document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.getElementById('about_me').addEventListener('click', function() {
+    document.querySelector('.bg-modals').style.display = 'flex';
+    });
+    document.querySelector('.close-btns').addEventListener('click', function(){
+    document.querySelector('.bg-modals').style.display = 'none';
+    });
